b2a9a81dca3d38062050a3d71150746ca2bf59d2
AnoushkaBhadra <anoushkabhadra@gmail.com>
2026-07-03 08:51:56 +0530

    Updating App.js (temp)

--- App.js
+++ App.js
@@ -2,15 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoadFonts from './src/theme/LoadFonts';
 
-import LoginScreen from './src/screens/Auth/LoginScreen';
+//import LoginScreen from './src/screens/Auth/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStack from './src/navigation/RootStack'
 export default function App() {
 
   const fontsLoaded = LoadFonts();
   if(!fontsLoaded) return null;
   return (
-    <>
-      <LoginScreen />
-    </>
+    <NavigationContainer>
+      <RootStack/>
+    </NavigationContainer>
   );
 }
 
